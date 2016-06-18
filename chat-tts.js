d45b7b4330d09b655351a865144eff971717b8cd
Andrew Chen <yongjhih@gmail.com>
2016-06-18 18:06:48 +0800

    Rename as alias

--- chat-tts.js
+++ chat-tts.js
@@ -35,7 +35,7 @@ channels = ['yongjhih', 'mistakelolz', 'twitchplayspokemon'], // Channels to ini
 	showHosting = true, // Show when the channel is hosting or not
 	showConnectionNotices = true; // Show messages like "Connected" and "Disconnected"
 }
-var channelName = qs['channel_name'] ? qs['channel_name'] : qs['channel'];
+var alias = qs['alias'] ? qs['alias'] : qs['channel'];
 
 var chat = document.getElementById('chat'),
 	defaultColors = ['rgb(255, 0, 0)','rgb(0, 0, 255)','rgb(0, 128, 0)','rgb(178, 34, 34)','rgb(255, 127, 80)','rgb(154, 205, 50)','rgb(255, 69, 0)','rgb(46, 139, 87)','rgb(218, 165, 32)','rgb(210, 105, 30)','rgb(95, 158, 160)','rgb(30, 144, 255)','rgb(255, 105, 180)','rgb(138, 43, 226)','rgb(0, 255, 127)'],
@@ -305,7 +305,7 @@ client.addListener('reconnect', function () {
 client.addListener('join', function (channel, username) {
 		if(joinAccounced.indexOf(channel) == -1) {
 			if(showConnectionNotices) chatNotice(capitalize(dehash(username)) + ' joined ' + capitalize(dehash(channel)), 1000, -1, 'chat-room-join');
-                        var welcomeMsg = '歡迎來到 ' + channelName + ' 的遊戲間 ' + username;
+                        var welcomeMsg = '歡迎來到 ' + alias + ' 的遊戲間 ' + username;
                         responsiveVoice.speak(welcomeMsg, 'Chinese Female');
                         var user;
                         user = {
