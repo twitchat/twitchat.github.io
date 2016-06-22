75351d8886f6c6dbfab9f4f643e8346dbccebbfa
Andrew Chen <yongjhih@gmail.com>
2016-06-22 21:52:27 +0800

    Avoid stay recorded before welcomeMsg

--- chat-tts-youtube.js
+++ chat-tts-youtube.js
@@ -514,7 +514,7 @@ client.on("join", function (channel, username, self) {
 
 
                                         var welcomeMsg;
-                                        if (stay_duration > 0) {
+                                        if (stay_duration > 10) {
                                             welcomeMsg = '歡迎再次來到 ' + alias + ' 的遊戲間 ' + username;
                                         } else {
                                             welcomeMsg = '歡迎第一次來到 ' + alias + ' 的遊戲間 ' + username;
